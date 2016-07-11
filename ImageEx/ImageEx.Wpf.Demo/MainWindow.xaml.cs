49d3b1a68a7872f80a791eef8959c0da74d8893f
h82258652 <h82258652@hotmail.com>
2016-07-11 09:01:25 +0800

    update

--- ImageEx/ImageEx.Wpf.Demo/MainWindow.xaml.cs
+++ ImageEx/ImageEx.Wpf.Demo/MainWindow.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace Controls.Wpf.Demo
+﻿using System.Windows;
+
+namespace Controls.Wpf.Demo
 {
     public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Image.Source == "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png")
+            {
+                Image.Source = null;
+            }
+            else
+            {
+                Image.Source =
+                    "https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo/bd_logo1_31bdc765.png";
+            }
         }
     }
 }
