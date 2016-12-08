546e3edba3f410b0da65e7606b1efd9743e03b8e
h82258652 <h82258652@hotmail.com>
2016-12-08 18:01:54 +0800

    测试

---
+++ ImageExV4/WpfApplication1/CustomControl1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    public class CustomControl1 : Control
+    {
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register(nameof(Source), typeof(string), typeof(CustomControl1), new PropertyMetadata(null));
+
+        public string Source
+        {
+            get
+            {
+                return GetValue(SourceProperty) as string;
+            }
+            set
+            {
+                SetValue(SourceProperty, value);
+            }
+        }
+
+        static CustomControl1()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
+        }
+    }
+}
